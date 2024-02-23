77a00f8b5f3f3ce85471021af5786ccc2f20946e
whd <didi@MacBook-Pro.local>
2024-02-23 17:56:04 +0800

    修改extension处理

--- buildSrc/src/main/java/com/example/buildsrc/model/CustomConfigExtension.kt
+++ buildSrc/src/main/java/com/example/buildsrc/model/CustomConfigExtension.kt
@@ -1,9 +1,31 @@
 package com.example.buildsrc.model
 
+import org.gradle.api.Action
 import java.io.File
 
 open class CustomConfigExtension {
+    // bool类型
     var enable = false
     var incremental = false
+    // 文件类型
     var config: File? = null
+
+    // 数组类型
+    internal var items1: MutableList<String> = mutableListOf()
+    // 自定义类型
+    internal val customItem = CustomItem()
+    // 自定义类型一定要用自定义方法上传
+    fun customItem(action: Action<CustomItem>) {
+        action.execute(customItem)
+    }
+
+    fun items(action:MutableList<String>) {
+        items1.addAll(action)
+    }
+
+}
+
+open class CustomItem {
+    var name = ""
+    var enable = false
 }
--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -29,8 +29,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:${Version.KotlinVersion}")
     implementation("com.android.tools.build:gradle:${Version.AgpVersion}")
 
-    implementation("com.google.code.gson:gson:2.8.2")
-    /* Example Dependency *//* Depend on the kotlin plugin, since we want to access it in our plugin */
+    implementation("com.google.code.gson:gson:2.8.2")/* Example Dependency *//* Depend on the kotlin plugin, since we want to access it in our plugin */
     implementation("org.javassist:javassist:3.28.0-GA")/* Depend on the default Gradle API's since we want to build a custom plugin */
 
 
@@ -48,6 +47,18 @@ afterEvaluate {
                 version = PluginConfig.Version
                 from(components["java"])
             }
+        }
+        repositories {
+            maven {
+                url = uri("../repo")
+            }
+//            maven {
+//                url = uri("http://www.baidu.com")
+//                credentials {
+//                    username = "Arirus.Wang"
+//                    password = "111"
+//                }
+//            }
         }
     }
 }
